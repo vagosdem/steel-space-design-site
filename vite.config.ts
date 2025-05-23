5ebf601539c78967f0c8608380f0e75dd15fed63
vagosdem <149492828+vagosdem@users.noreply.github.com>
2025-05-23 22:38:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/steel-space-design-site/", 
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
- mode === 'development' &&
-    componentTagger()
+    mode === "development" && componentTagger()
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -21,7 +21,6 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     cssCodeSplit: true,
-    rollupOptions: {
-    },
+    rollupOptions: {},
   },
 }));
